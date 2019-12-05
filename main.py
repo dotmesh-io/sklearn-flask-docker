0880f5ab3a5428b2d00260ee5b7fc4877c6275cc
Karolis Rusenas <karolis.rusenas@gmail.com>
2019-12-05 17:36:12 +0000

    prediction endpoint, loading model once

--- main.py
+++ main.py
@@ -1,26 +1,51 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from joblib import load
+import numpy as np
 import os
 import http
 import sys
+
 app = Flask(__name__)
 
+model = None
 
 @app.route('/healthcheck')
 def healthcheck():
     return 'true'
 
-@app.route('/predict')
+@app.route('/predict', methods=['POST'])
 def predict():
-    if request.args.get("parameters", None) == None:
-        return {"error": "did not supply any parameters"}, http.HTTPStatus.BAD_REQUEST
-    model = load(os.environ.get("MODEL_JOBLIB_FILE", "/app/model.joblib"))
-    return model.predict(request.args["parameters"])
+    data = request.get_json(force=True)
+
+    print(data)
+
+    instances = data["instances"]
+    try:
+        inputs = np.array(instances)
+    except Exception as e:
+        raise Exception(
+            "Failed to initialize NumPy array from inputs: %s, %s" % (e, instances))
+    try:
+        result = model.predict(inputs).tolist()
+        return { "predictions" : result }
+    except Exception as e:
+            raise Exception("Failed to predict %s" % e)
 
 if __name__ == '__main__':
     host = os.environ.get("FLASK_HOST", "0.0.0.0")
     port = os.environ.get("FLASK_PORT", "8501")
     # if we haven't overriden the model filename using env var at runtime, use the args passed in
-    if len(sys.argv) > 0:
+    if len(sys.argv) > 1:
         os.environ.setdefault("MODEL_JOBLIB_FILE", sys.argv[1])
+
+    try:
+        model = load(os.environ.get("MODEL_JOBLIB_FILE", "example_model/model.joblib"))
+        print('model loaded')
+
+    except Exception as e:
+        print('No model here')
+        print('Train first')
+        print(str(e))
+        sys.exit()
+
     app.run(host=host, port=port)
